0a901149fc8d07b56f4d2e71037a164b0c31d8df
Juergen Helmers <juergen.helmers@gmail.com>
2011-09-29 11:33:34 +0200

    moved most views to haml

--- db/migrate/20110314233516_add_newsletter_subscribe_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddNewsletterSubscribeToUsers < ActiveRecord::Migration
-  def self.up
-    add_column :users, :newsletter_subscribe, :boolean
-  end
-
-  def self.down
-    remove_column :users, :newsletter_subscribe
-  end
-end
